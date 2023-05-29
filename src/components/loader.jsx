123d488fdc44c842120e7770b892b9523d915a10
Atharv Vani <atharvavani110@gmail.com>
2023-05-30 03:53:49 +0530

    first commit

---
+++ src/components/loader.jsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from "react";
+import "../App.css";
+import PropagateLoader from "react-spinners/PropagateLoader";
+
+
+export default function About() {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [])
+    return (
+    <>
+        <PropagateLoader
+          color="#0064ff"
+          loading={loading}
+          size={40}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+    </>
+  );
+}
