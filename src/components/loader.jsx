0043c3483c02118d7579e86c3d84b9ab13ad27b7
Atharv Vani <atharvavani110@gmail.com>
2023-05-30 13:51:33 +0530

    Optimised code

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -4,18 +4,18 @@ import PropagateLoader from "react-spinners/PropagateLoader";
 
 
 export default function About() {
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 3000)
-    }, [])
+    // const [loading, setLoading] = useState(false);
+    // useEffect(() => {
+    //     setLoading(true)
+    //     setTimeout(() => {
+    //         setLoading(false)
+    //     }, 3000)
+    // }, [])
     return (
     <>
         <PropagateLoader
           color="#0064ff"
-          loading={loading}
+          loading={true}
           size={40}
           aria-label="Loading Spinner"
           data-testid="loader"
